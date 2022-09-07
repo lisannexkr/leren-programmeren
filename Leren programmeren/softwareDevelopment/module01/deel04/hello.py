51e69181b74414f3da677d51f5f5f0b71776e6c6
Lisanne <99072683@mydavinci>
2022-09-07 11:37:19 +0200

    herorganisatie

---
+++ Leren programmeren/softwareDevelopment/module01/deel04/hello.py
@@ -0,0 +1,9 @@
+print('Hello wereld van')
+print('pppp    y     y    ttttttt    h   h    oooo    nnnn')
+print('p   p    y   y        t       h   h   o    o   n   n')
+print('p   p     y y         t       h   h   o    o   n   n')
+print('pppp       y          t       hhhhh   o    o   n   n')
+print('p          y          t       h   h   o    o   n   n')
+print('p          y          t       h   h   o    o   n   n')
+print('p          y          t       h   h    oooo    n   n')
+# lisanne van de wege 99072683
