21b7efdbaba0ad1c2a401adb488dcc4644fcd2a8
Lisanne <99072683@mydavinci>
2022-09-12 10:23:26 +0200

    variabelen aangepast

--- Leren programmeren/softwareDevelopment/module02/deelA/feestlunch.py
+++ Leren programmeren/softwareDevelopment/module02/deelA/feestlunch.py
@@ -1,18 +1,18 @@
-k = 17
+croissantjes = 17
 l = 1.39
 
-o = 2
+stokbroodjes = 2
 a = 2.78
 
-x = 3
+kortingen = 3
 q = 3.43
 
-croissant = (k*l)
-stokbrood = (o*a)
-korting = (x*q)
+croissant = (croissantjes*l)
+stokbrood = (stokbroodjes*a)
+korting = (kortingen*q)
 
 print("De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
-print("Je bent {} euro kwijt voor de croissantjes".format(k*l))
-print("Ook ben je {} euro kwijt aan de stokbroodjes".format(o*a))
-print("Maar je hebt 3 kortingsbonnen die bij elkaar {} euro waard zijn".format(x*q))
+print("Je bent {} euro kwijt voor de croissantjes".format(croissantjes*l))
+print("Ook ben je {} euro kwijt aan de stokbroodjes".format(stokbroodjes*a))
+print("Maar je hebt 3 kortingsbonnen die bij elkaar {} euro waard zijn".format(kortingen*q))
 print("Uiteindelijk moet je dan {} betalen voor alles!".format(croissant+stokbrood-korting))
