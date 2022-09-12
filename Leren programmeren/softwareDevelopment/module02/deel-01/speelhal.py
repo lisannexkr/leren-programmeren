f0b80d96f5cffddb4b85b2f607b4375e4280e2bd
Lisanne <99072683@mydavinci>
2022-09-12 10:11:41 +0200

    speelhal 2.0

--- Leren programmeren/softwareDevelopment/module02/deel-01/speelhal.py
+++ Leren programmeren/softwareDevelopment/module02/deel-01/speelhal.py
@@ -4,9 +4,9 @@ vipvr = 0.37
 minuten = 45
 minuten2 = 5
 ticketsheledag = 29.8
-ticketsvipvr = 13.32
+ticketsvipvr = 14.60
 
-print("de rekening van de speelhal is")
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro")
 print("Voor tickets voor de hele dag betaal je {} met 4 mensen".format(personen*toegangsticket))
 print("En als je de VIP-VR Game-seat wilt doen dan is het {} ".format(minuten/minuten2*vipvr*personen))
 print("Dus dan ben je {} kwijt voor de hele dag".format(ticketsheledag+ticketsvipvr))
