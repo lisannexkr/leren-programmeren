5ab913b694c7310a2721d44cc4bd13e876a09ada
Lisanne <99072683@mydavinci>
2022-09-14 11:40:31 +0200

    herorganisatie

---
+++ Leren programmeren/softwareDevelopment/module02/test.py
@@ -0,0 +1,17 @@
+# werkt niet
+
+#antwoord2 = input("Wil je een medium pizza voor €7?").lower()
+#if antwoord2 == 'ja':
+    #antwoord2=input('Hoeveel wil je er?')
+    #antwoord2=int(antwoord2)
+    #if antwoord2 == float:
+   #     print("Sorry! Je kan geen halve pizza's bestellen.")
+  #  else:
+ #       print("Oke,")
+
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
