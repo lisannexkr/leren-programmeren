ba775a12b64faeed1110d50ab45b1b1511179692
Lisanne <99072683@mydavinci>
2022-09-14 11:40:05 +0200

    min en max (herorganisatie naar goede map)

---
+++ Leren programmeren/softwareDevelopment/module02/deel03/metenIsWeten.py
@@ -0,0 +1,15 @@
+a = input("Wat is A?") 
+b = input("Wat is B?")
+if b > a:
+  print("A is het kleinste getal, {}".format(a))
+  min = a
+elif a < b:
+  print("")
+else:
+  print("A is even groot als B")
+
+min = a
+max = b
+
+print("Het minimum is {}".format())
+print("Het maximum is {}".format())
