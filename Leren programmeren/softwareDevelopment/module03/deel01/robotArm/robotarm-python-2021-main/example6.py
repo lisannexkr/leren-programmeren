73e1db937cca8d5559a72aafc162f120f0435c5d
Lisanne <99072683@mydavinci>
2022-10-20 13:55:02 +0200

    RobotArm Exercise 6

---
+++ Leren programmeren/softwareDevelopment/module03/deel01/robotArm/robotarm-python-2021-main/example6.py
@@ -0,0 +1,21 @@
+# Oefening 6
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+for i in range(0,3):
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
+
+# Verdeel de rode en witte blokken over 2 stapels: een rode stapel en een witte stapel. 
