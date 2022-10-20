80be76a4b0b981e0e5ede0f7e7dafab11b2e696c
Lisanne <99072683@mydavinci>
2022-10-20 11:30:30 +0200

    RobotArm Exercise 1

--- Leren programmeren/softwareDevelopment/module03/deel01/robotArm/robotarm-python-2021-main/example1.py
+++ Leren programmeren/softwareDevelopment/module03/deel01/robotArm/robotarm-python-2021-main/example1.py
@@ -1,21 +1,11 @@
-## Oefening 1
+# Oefening 1
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 1')
 
-# Jouw python instructies zet je vanaf hier:
 robotArm.moveRight()
-
 robotArm.grab()
-
 robotArm.moveLeft()
-
 robotArm.drop()
 
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
-
-
-#Verplaats het rode blok één plek naar links.
-
-#![Oefening 1](readme/exercise1.png)
