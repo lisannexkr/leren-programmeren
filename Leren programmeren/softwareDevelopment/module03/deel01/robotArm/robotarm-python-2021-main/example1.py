cd530185730414308563d020d4633ab889c1eb8a
Lisanne <99072683@mydavinci>
2022-10-20 11:04:37 +0200

    RobotArm Excersise 1

---
+++ Leren programmeren/softwareDevelopment/module03/deel01/robotArm/robotarm-python-2021-main/example1.py
@@ -0,0 +1,21 @@
+## Oefening 1
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 1')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+
+robotArm.grab()
+
+robotArm.moveLeft()
+
+robotArm.drop()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
+
+
+#Verplaats het rode blok één plek naar links.
+
+#![Oefening 1](readme/exercise1.png)
