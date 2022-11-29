2d9c75f0b8c072c1d1f02bea4cf2af3dacc365c8
Lisanne <99072683@mydavinci>
2022-11-29 15:02:56 +0100

    tuple

---
+++ Leren programmeren/softwareDevelopment/module04/deel01/days.py
@@ -0,0 +1,13 @@
+dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+
+werkDagen = dagen[0:5]
+weekendDagen = dagen[5:7]
+dagenReversed = list(reversed(dagen))
+werkDagenReversed = list(reversed(werkDagen))
+weekendDagenReversed = list(reversed(weekendDagen))
+
+print(werkDagen)
+print(weekendDagen)
+print(dagenReversed)
+print(werkDagenReversed)
+print(weekendDagenReversed)
