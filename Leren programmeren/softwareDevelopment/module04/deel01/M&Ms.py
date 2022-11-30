55fc58f9531a1a1b2a79efee856bc32d22d6ce9d
Lisanne <99072683@mydavinci>
2022-11-30 11:44:06 +0100

    bezig met programma

--- Leren programmeren/softwareDevelopment/module04/deel01/M&Ms.py
+++ Leren programmeren/softwareDevelopment/module04/deel01/M&Ms.py
@@ -1,5 +1,15 @@
-MenMkleuren = ("oranje", "groen", "blauw", "bruin")
+MenMkleuren = ["oranje", "groen", "blauw", "bruin"]
 hoeveelMenMs = int(input("Hoeveel M&M's moeten er toegevoegd worden? "))
-print(hoeveelMenMs)
 
+# for x in range (1):
+#     MenMkleuren.append()
+#     MenMkleuren.randomchoice
+i = 0
+for i in range (hoeveelMenMs):
+    i = i + 1
+    MenMkleuren.append(f"append + {i}")
+
+
+
+print(MenMkleuren)
 
