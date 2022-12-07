f84a975b7e9a42df0c3c95e1f7778f93eaa8766f
Lisanne <99072683@mydavinci>
2022-12-07 11:06:19 +0100

    kaarten programma afgemaakt

--- Leren programmeren/softwareDevelopment/module04/deel01/kaarten.py
+++ Leren programmeren/softwareDevelopment/module04/deel01/kaarten.py
@@ -1,13 +1,18 @@
 import random
 
-kaarten = [2,3,4,5,6,7,8,9,10,"boer", "vrouw", "heer", "aas"]
-kaartkleur = ["harten", "klaveren", "schoppen", "ruiten", "joker", "joker"]
+kaarten = ["2","3","4","5","6","7","8","9","10","boer", "vrouw", "heer", "aas"]
+kaartkleur = ["harten", "klaveren", "schoppen", "ruiten"]
+jokers = 2
+deck = []
 
-y = 1
-while y <= 7:
-    if (random.choice(kaartkleur)) == 'joker':
-        print(f"Kaart {y}: joker")
-        y = y + 1
-    else:
-        print(f"Kaart {y}: {random.choice(kaartkleur)} {random.choice(kaarten)}")
-        y = y + 1
+for i in range (len(kaartkleur)):
+    for j in range (len(kaarten)):
+        deck.append(f"{kaartkleur[i]} {kaarten[j]}")
+
+kaartnummer = 1
+for i in range (7):
+    random.shuffle(deck)
+    print(f"Kaart {kaartnummer}; {deck.pop(0)}")
+    kaartnummer = kaartnummer + 1
+print("")
+print(f"deck (47 kaarten): {deck}")
